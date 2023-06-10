85b503bafce77765c418f14fcd1968a69b43e16b
Anne <anne_hensel@web.de>
2023-06-10 16:25:00 +0200

    table updates

--- sql-training.sql
+++ sql-training.sql
@@ -1,1 +1,14 @@
-SELECT * FROM topten
+CREATE DATABASE newDB;
+
+CREATE TABLE vendors (
+    Model varchar(255),
+    SKU varchar(255),
+    Categories varchar(255),
+    VName varchar(255),
+    VDescription varchar(1000)
+)
+
+INSERT INTO vendors
+VALUES ('A102-GripBleistift', 'A102-100-000-015', 'OFFICE/PENCILS_CRAYONS_MARKERS', 'Bleistift Grip 2001 schwarz', 'Grip Bleistifte aus FSC-zertifiziertem Holz, Politur auf umweltfreundlicher Wasserlackbasis, patentierte Soft-Grip-Zone (Noppen), ergonomische Dreieckform fuer ermuedungsfreies Schreiben und Zeichnen, bruchgeschuetzt durch Sekuralverleimung, getauchtes Stiftende, Haertegrad: HB, Laenge: 175 mm. Lose zu 100 oder 300 Stueck im Karton verpackt.');
+
+SELECT * FROM vendors;
